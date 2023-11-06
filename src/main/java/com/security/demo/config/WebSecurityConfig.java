ba408bb71fe4752a3e9f4074c77fa43f8d711576
brunoclaumari <brunoclaumari@gmail.com>
2023-11-06 03:34:34 -0300

    Início do estudo de Spring Security

---
+++ src/main/java/com/security/demo/config/WebSecurityConfig.java
@@ -0,0 +1,59 @@
+package com.security.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+/*
+ * Exemplo de autenticação na memória (In Memory Authentication)
+ * Não está usando a herança do "WebSecurityConfigurerAdapter por que ele está obsoleto,
+ * então os métodos abaixo são Beans que devem suprir o comportamento esperado dessa
+ * classe de configuração de autenticação
+ * */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig  {
+	
+	@Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests()
+        .antMatchers("/").permitAll()
+        .antMatchers(HttpMethod.POST, "/login").permitAll()
+        .antMatchers("/managers").hasAnyRole("MANAGERS")
+        .antMatchers("/users").hasAnyRole("USERS","MANAGERS")
+        .anyRequest().authenticated().and().formLogin();        
+        
+        return http.build();
+    }
+	
+	@Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+	    manager.createUser(User.withUsername("user")
+	      .password(passwordEncoder().encode("user123"))
+	      .roles("USERS")
+	      .build());
+	    manager.createUser(User.withUsername("admin")
+	      .password(passwordEncoder().encode("master123"))
+	      .roles("MANAGERS")
+	      .build());
+	    return manager;
+    }
+	
+	@Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }	
+
+}
